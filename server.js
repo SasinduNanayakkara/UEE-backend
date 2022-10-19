1d401f43ba5e4cdc6601522fddfeba33ab54d5f7
SasinduNanayakkara <it20625016@my.sliit.lk>
2022-10-20 00:19:09 +0530

    Define new routes

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ connectDB();
 
 app.get("/", (req,res) => res.send("Hello World!"));
 
+app.use("/api/admin")
+app.use("/api/investor")
+app.use("/api/inventor")
+app.use("/api/organization")
+app.use("/api/investment")
+app.use("/api/invention")
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
