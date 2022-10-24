608f6657860f6583b3f71f6b82b568d2e7d1a4fb
Shavidini Ekanayake <77123486+ShavidiniEkanayake@users.noreply.github.com>
2022-10-25 00:26:37 +0530

    Added invention controller

---
+++ controllers/invention.controller.js
@@ -0,0 +1,9 @@
+var express = require('express');
+const { isValidObjectId } = require('mongoose');
+var router = express.Router();
+var mongoose = require('mongoose');
+const inventionModel = require('../models/invention');
+
+module.exports = {
+    
+  }
