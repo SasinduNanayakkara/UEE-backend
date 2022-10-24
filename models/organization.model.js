7141fa418071f277fb187167a64f09155567c245
Ravindu Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2022-10-25 03:04:58 +0530

    organization model added

---
+++ models/organization.model.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+// Declare the Schema of the Mongo model
+const organizationSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  orgName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  app_nic: {
+    type: String,
+    required: true,
+  },
+  app_name: {
+    type: String,
+    required: true,
+  },
+  app_nic_img: {
+    type: String,
+    required: true,
+  },
+  app_position: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  website: {
+    type: String,
+    required: true,
+  },
+  approval: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+//Export the model
+module.exports = mongoose.model("Organization", organizationSchema);
