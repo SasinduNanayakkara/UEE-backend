6bce5637b25b940b8cfb04ce854acfeda2f6fc40
SasinduNanayakkara <it20625016@my.sliit.lk>
2022-10-25 21:45:19 +0530

    Define investor routes

--- routes/investor.routes.js
+++ routes/investor.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { addInvestor, getInvestors, getOneInvestor, updateInvestor, deleteInvestor } = require('../controllers/investor.controller');
+const { addInvestor, getInvestors, getOneInvestor, updateInvestor, deleteInvestor, addInvention } = require('../controllers/investor.controller');
 
 
 router.post("/", addInvestor);
@@ -7,5 +7,6 @@ router.get("/", getInvestors);
 router.get("/:id", getOneInvestor);
 router.put("/:id", updateInvestor);
 router.delete("/:id", deleteInvestor);
+router.put("/invention/:id", addInvention);
 
 module.exports = router;
