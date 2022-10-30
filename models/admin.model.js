e4e843616aa12973e059dd059705d28e44ac0f46
Susith Rupasinghe <susith03program@gmail.com>
2022-10-31 00:27:01 +0530

    model added

---
+++ models/admin.model.js
@@ -0,0 +1,22 @@
+const { Schema } = require("mongoose");
+
+const adminSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Admin', adminSchema);
