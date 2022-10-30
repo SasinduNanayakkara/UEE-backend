e886f1ea9cfc1de1a36d77f18b9f6ac3832319f6
Susith Rupasinghe <susith03program@gmail.com>
2022-10-31 00:27:15 +0530

    routes added

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,8 +1,9 @@
+const { addAdmin, getOneAdmin, updateAdmin } = require('../controllers/admin.controller');
+
 const router = require('express').Router();
 
-router.get("/", getAllAdmins);
-router.get("/:id", getAdminById);
+router.create("/", addAdmin);
+router.get("/:id", getOneAdmin);
 router.put("/:id", updateAdmin);
-router.delete("/:id", deleteAdmin);
 
 module.export = router;
