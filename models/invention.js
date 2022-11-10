1d7352184e8b558da4219fa03a4efd33299ee7c6
Shavidini Ekanayake <77123486+ShavidiniEkanayake@users.noreply.github.com>
2022-11-11 00:31:11 +0530

    added records

--- models/invention.js
+++ models/invention.js
@@ -13,38 +13,38 @@ const InventionSchema = new mongoose.Schema({
     required: true,
     type: String,
   },
-  // image: {
-  //   required: true,
-  //   type: String,
-  // },
-  // date: {
-  //   required: true,
-  //   type: Number,
-  // },
-  // goal: {
-  //   required: true,
-  //   type: String,
-  // },
-  // currentInvestment: {
-  //   required: true,
-  //   type: Number,
-  // },
-  // approval: {
-  //   required: true,
-  //   type: Boolean,
-  // },
-  // organization: {
-  //   required: true,
-  //   type: String,
-  // },
-  // investment: {
-  //   required: true,
-  //   type: String,
-  // },
-  // rate: {
-  //   required: true,
-  //   type: Boolean,
-  // },
+  image: {
+    required: true,
+    type: String,
+  },
+  date: {
+    required: true,
+    type: Number,
+  },
+  goal: {
+    required: true,
+    type: String,
+  },
+  currentInvestment: {
+    required: true,
+    type: Number,
+  },
+  approval: {
+    required: true,
+    type: Boolean,
+  },
+  organization: {
+    required: true,
+    type: String,
+  },
+  investment: {
+    required: true,
+    type: String,
+  },
+  rate: {
+    required: true,
+    type: Boolean,
+  },
 });
 
 module.exports = mongoose.model("invention", InventionSchema);
