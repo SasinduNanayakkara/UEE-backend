1ddaf6d2c3dbf9869b742103d01cd502faabe8cf
Ravindu Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2022-11-12 21:29:20 +0530

    get requests added

--- routes/organization.routes.js
+++ routes/organization.routes.js
@@ -9,15 +9,18 @@ const {
   rejectedToOrg,
   getAllOrgs,
   getOrgById,
+  getRequests,
 } = require("../controllers/organization.controller");
 
 router.post("/", registerOrg);
 router.put("/add/:id", addToOrg);
 router.put("/request/:id", requestToOrg);
+router.get("/request/:id", getRequests);
 router.put("/approval/:id", approvalToOrg);
 router.put("/rejected/:id", rejectedToOrg);
 router.get("/", getAllOrgs);
 router.get("/:id", getOrgById);
+
 // router.put("/:id", updateOrg);
 // router.delete("/:id", deleteOrg);
 
