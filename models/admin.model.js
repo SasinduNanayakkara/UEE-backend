38d4ecbb95050fe42716dbe7d9f19524927bdbd2
Susith Rupasinghe <susith03program@gmail.com>
2022-11-13 13:05:44 +0530

    Admin model added

--- models/admin.model.js
+++ models/admin.model.js
@@ -1,22 +1,24 @@
-const { Schema } = require("mongoose");
 const mongoose = require("mongoose");
-const adminSchema = new Schema({
+const adminSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     username: {
-        type: String,
-        required: true,
-    }
-}, { timestamps: true });
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Admin', adminSchema);
+module.exports = mongoose.model("Admin", adminSchema);
